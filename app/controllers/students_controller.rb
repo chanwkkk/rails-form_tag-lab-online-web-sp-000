84745ab776606dfb6ae45b7eccb42b63730affa6
Yingqi Chen <chanwk18@hotmail.com>
2019-09-13 20:24:19 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,28 +1,24 @@
 class StudentsController < ApplicationController
 
   def index
-    puts 'indexing'
     @students = Student.all
   end
 
   def show
-    puts 'showing...'
     @student = Student.find(params[:id])
   end
 
   def new
-    puts 'newing...'
   end
 
   def create
-    puts'creating'
-    session[:form_params] = params.inspect
-    redirect_to new_student_path
+    @student=Student.create(user_params)
+    redirect_to '/students'
   end
 
   private
 
-    def set_student
-      @student = Student.find(params[:id])
-    end
+ def user_params
+   params.require(:student).permit(:first_name, :last_name)
+ end
 end
