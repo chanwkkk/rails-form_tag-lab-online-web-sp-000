20fe8686f0be52bd620d7e421bcb4b09c08263ba
Yingqi Chen <chanwk18@hotmail.com>
2019-09-13 20:22:44 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,17 +1,21 @@
 class StudentsController < ApplicationController
 
   def index
+    puts 'indexing'
     @students = Student.all
   end
 
   def show
+    puts 'showing...'
     @student = Student.find(params[:id])
   end
 
   def new
+    puts 'newing...'
   end
 
   def create
+    puts'creating'
     session[:form_params] = params.inspect
     redirect_to new_student_path
   end
